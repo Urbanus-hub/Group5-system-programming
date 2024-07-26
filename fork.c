800b1fe10e71a5fdc2c76445633767530da6f3c2
Cynti84 <115475111+Cynti84@users.noreply.github.com>
2024-07-26 13:02:58 +0300

    fork.c

--- fork.c
+++ fork.c
@@ -12,3 +12,15 @@
       int main(){
        forkInstance();
 }
+
+/*Summary
+fork(): Creates a new process. The new process (child) runs concurrently with the original process (parent).
+Child Process: Executes the printf("Hello from child\n"); line.
+Parent Process: Executes the printf("Hello from parent\n"); line.
+Output:
+Hello from parent
+Hello from child
+Or
+Hello from child
+Hello from parent
+*/
