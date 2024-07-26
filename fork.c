e0bb9af4952a27da6d4381ec65751db717bd7ea4
894risper <145770911+894risper@users.noreply.github.com>
2024-07-26 12:09:15 +0300

    System call: fork function

--- fork.c
+++ fork.c
@@ -3,8 +3,10 @@
 #include <unistd.h>
    void forkInstance(){
      if (fork()==0){
+        //child
      printf("Hello from child");
        else
+        //parent
         printf("Hello from parent");
       }
       int main(){
